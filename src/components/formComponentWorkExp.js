ef4c466278f134ada90ad874668267c97e6c8b9d
mubzie <85705074+mubzie@users.noreply.github.com>
2023-08-01 13:18:37 +0100

    worked on conditional render of workexp array. add new workexp btn will show if workexp array is equal to zero

--- src/components/formComponentWorkExp.js
+++ src/components/formComponentWorkExp.js
@@ -22,29 +22,54 @@ class WorkExperience extends Component {
           "I was the lead Engineer on the android project. Leading a team of 100 software engineers thought-out the entire project lifecylce",
       },
 
+      dropDown: false,
+
       showForm: false,
 
-      workExperiences: [],
+      showForm2: false,
+
+      workExperiences: [
+        // {
+        //   company: "Google",
+        //   role: "Software Engineer",
+        //   startDate: "07/2022",
+        //   endDate: "present",
+        //   id: uniqid(),
+        //   city: "Silicon Valley",
+        //   country: "USA",
+        //   profile:
+        //     "I was the lead Engineer on the android project. Leading a team of 100 software engineers thought-out the entire project lifecylce",
+        // },
+      ],
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.onSubmitExperience = this.onSubmitExperience.bind(this);
     this.deleteWorkExperience = this.deleteWorkExperience.bind(this);
     this.onClearButton = this.onClearButton.bind(this);
-    this.toggleForm = this.toggleForm.bind(this);
-    // this.checkArrayLength = this.checkArrayLength.bind(this)
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+    // this.toggleForm = this.toggleForm.bind(this);
+    this.displayForm = this.displayForm.bind(this);
   }
 
-  checkArrayLength() {
-    if (this.state.workExperiences.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
+  toggleDropDown() {
+    this.setState({
+      dropDown: !this.state.dropDown,
+      showForm: !this.state.showForm,
+      showForm2: false,
+    });
   }
 
-  toggleForm() {
-    this.setState({ showForm: !this.state.showForm });
+  // toggleForm() {
+  //   this.setState({ showForm: !this.state.showForm });
+  // }
+
+  displayForm() {
+    this.setState({
+      showForm2: true,
+      showForm: false,
+      dropDown: !this.state.dropDown,
+    });
   }
 
   handleChange(e) {
@@ -108,19 +133,35 @@ class WorkExperience extends Component {
   }
 
   render() {
-    const { workExperience, showForm } = this.state;
+    const { workExperience, dropDown, showForm, showForm2 } = this.state;
     const { formTitle } = this.props;
 
     return (
       <div className="form-container-workexp">
         <div className="form-header-container">
           <div className="form-title">{formTitle}</div>
-          <div onClick={this.toggleForm} className="icon">
-            {showForm ? <HiOutlineChevronDown /> : <HiOutlineChevronRight />}
+          <div onClick={this.toggleDropDown} className="icon">
+            {dropDown ? <HiOutlineChevronDown /> : <HiOutlineChevronRight />}
           </div>
         </div>
 
         {showForm && (
+          <>
+            {this.state.workExperiences.length === 0 && (
+              <div>
+                <div>
+                  this array is empty {this.state.workExperiences.length}
+                </div>
+                <button className="add-new-button" onClick={this.displayForm}>
+                  {" "}
+                  Add work experience{" "}
+                </button>
+              </div>
+            )}
+          </>
+        )}
+
+        {showForm2 && (
           <>
             <form onSubmit={this.onSubmitExperience}>
               <input
@@ -221,117 +262,6 @@ class WorkExperience extends Component {
           </>
         )}
       </div>
-
-      // <div className="form-container-workexp">
-      //   <div className="form-header-container">
-      //     <div className="form-title">{formTitle}</div>
-      //     <div onClick={this.toggleForm} className="icon">
-      //       {showForm ? <HiOutlineChevronDown /> : <HiOutlineChevronRight />}
-      //     </div>
-      //   </div>
-
-      //   {showForm && (
-      //     <>
-      //       <form onSubmit={this.onSubmitExperience}>
-      //         <input
-      //           type="text"
-      //           placeholder="Company"
-      //           name="company"
-      //           id="nameInput"
-      //           onChange={this.handleChange}
-      //           value={workExperience.company}
-      //         ></input>
-      //         <input
-      //           type="text"
-      //           placeholder="Role"
-      //           name="role"
-      //           id="occupationInput"
-      //           onChange={this.handleChange}
-      //           value={workExperience.role}
-      //         ></input>
-
-      //         <div className="info-container">
-      //           <div className="info-title">
-      //             <label htmlFor="startDateInput">start Date</label>
-      //             <input
-      //               type="text"
-      //               placeholder="Start Date"
-      //               name="startDate"
-      //               id="startDateInput"
-      //               onChange={this.handleChange}
-      //               value={workExperience.startDate}
-      //             ></input>
-      //           </div>
-      //           <div className="info-title">
-      //             <label htmlFor="endDateInput">end Date</label>
-      //             <input
-      //               type="text"
-      //               placeholder="End Date"
-      //               name="endDate"
-      //               id="endDateInput"
-      //               onChange={this.handleChange}
-      //               value={workExperience.endDate}
-      //             ></input>
-      //           </div>
-      //         </div>
-
-      //         <div className="info-container">
-      //           <div className="info-title">
-      //             <label htmlFor="startDateInput">city</label>
-      //             <input
-      //               type="text"
-      //               placeholder="city"
-      //               name="city"
-      //               id="cityInput"
-      //               onChange={this.handleChange}
-      //               value={workExperience.city}
-      //             ></input>
-      //           </div>
-      //           <div className="info-title">
-      //             <label htmlFor="endDateInput">country</label>
-      //             <input
-      //               type="text"
-      //               placeholder="country"
-      //               name="country"
-      //               id="countryInput"
-      //               onChange={this.handleChange}
-      //               value={workExperience.country}
-      //             ></input>
-      //           </div>
-      //         </div>
-
-      //         <textarea
-      //           rows="3"
-      //           cols="30"
-      //           placeholder="write short description about your role in this company"
-      //           name="profile"
-      //           id="profileInput"
-      //           onChange={this.handleChange}
-      //           value={workExperience.profile}
-      //         ></textarea>
-      //       </form>
-
-      //       <div className="form-btns">
-      //         <div className="btn-title">
-      //           <button
-      //             className="delete-btn"
-      //             onClick={this.deleteWorkExperience}
-      //           >
-      //             Delete
-      //           </button>
-      //           <button className="clear-btn" onClick={this.onClearButton}>
-      //             Cancel
-      //           </button>
-      //         </div>
-
-      //         <button className="save-btn" onClick={this.onSubmitExperience}>
-      //           Save
-      //         </button>
-      //       </div>
-      //     </>
-      //   )}
-
-      // </div>
     );
   }
 }
